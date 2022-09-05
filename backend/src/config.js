34656ec757c0e0cf8abe8d8dd51d7ee2e22f84c4
Rodrigo <rodrigolfranca@gmail.com>
2022-09-05 04:25:10 -0300

    add: .env variables config boost and pg conect

--- backend/src/config.js
+++ backend/src/config.js
@@ -6,7 +6,12 @@ const config = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   BD_URL: process.env.BD_URL,
-  API_KEY: process.env.API_KEY
+  API_KEY: process.env.API_KEY,
+  DB_USER: process.env.DB_USER,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_HOST: process.env.DB_HOST,
+  DB_PORT: process.env.DB_PORT,
+  DB_DATABASE: process.env.DB_DATABASE
 };
 
 export default config;
