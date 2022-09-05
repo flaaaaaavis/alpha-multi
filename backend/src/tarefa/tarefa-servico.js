9a83b8705896f0603b225f050ec4977a58b486a0
Rodrigo <rodrigolfranca@gmail.com>
2022-09-05 12:32:44 -0300

    build: tarefa-servico

---
+++ backend/src/tarefa/tarefa-servico.js
@@ -0,0 +1,67 @@
+import { pool } from "../db";
+import config from "../config";
+
+export const TaskService = {
+    async getTarefasPorColuna(coluna_id) {
+
+        try {
+
+            const data = await pool.query(`SELECT * FROM tarefas WHERE coluna_id = ${coluna_id}`)
+            return data.rows;
+
+        } catch(e) {
+
+            console.log(e)
+
+        }
+
+    },
+    async insertTarefa(tarefa) {
+
+        try {
+
+            const query = `INSERT INTO tarefas (coluna_id, data_criacao, nome, ordem, tags, anotacoes) VALUES ($1, CURRENT_TIMESTAMP, $3, $4, $5, $6)`;
+            const values = [tarefa.coluna_id, tarefa.nome, tarefa.ordem, tarefa.tags, tarefa.anotacoes];
+            const data = await pool.query(query, values)
+            return data.rows;
+
+        } catch(e) {
+
+            console.log(e)
+
+        }
+
+    },
+    async updateTarefa(id, tarefa) {
+
+        try {           
+
+            const query = `UPDATE tarefas SET coluna_id = ($1), nome = ($2), ordem = ($3), tags = ($4), anotacoes = ($5)  WHERE id = ($6);`;
+            const values = [tarefa.coluna_id, tarefa.nome, tarefa.ordem, tarefa.tags, tarefa.anotacoes, id];
+            const data = await pool.query(query, values)
+            return data.rows;
+
+        } catch(e) {
+
+            console.log(e)
+
+        }
+        
+
+    },
+    async deleteTask(id) {
+
+        try {
+
+            const data = await pool.query(`DELETE FROM tarefas WHERE id = ${id}`)
+            return data.rows;
+
+        } catch(e) {
+
+            console.log(e)
+
+        }
+
+    },
+  };
+  
