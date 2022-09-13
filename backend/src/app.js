e8e359c835aa5c5fd7b788ad3bd05240f10be58d
Rodrigo <rodrigolfranca@gmail.com>
2022-09-13 16:06:51 -0300

    hotfix: app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,10 @@
 import TaskRouter from "./tarefa/tarefa-roteador.js";
+import CategoryRouter from "./categoria/categoria-roteador.js";
+import ProjectRouter from "./projeto/projeto-roteador.js";
 import express from "express";
 import cors from "cors";
-import CategoryRouter from "./categoria/categoria-roteador.js"
-import ProjectRouter from "./categoria/projeto-roteador.js"
-import TaskRouter from "./categoria/tarefa-roteador.js"
 
 export const app = express();
-
-app.use("/api/categoria", CategoryRouter);
-app.use("/api/projeto", ProjectRouter);
-app.use("/api/tarefa", TaskRouter);
 
 app.use(cors());
 
@@ -17,4 +12,6 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.use("/api/categoria", CategoryRouter);
+app.use("/api/projeto", ProjectRouter);
 app.use("/api/tarefa", TaskRouter);
