c7cdea2ea4de605c1d64d08eda8e62799dc95709
Rodrigo <rodrigolfranca@gmail.com>
2022-09-17 15:02:58 -0300

    minor fix: getUsuarioByEmail

--- backend/src/usuario/usuario-servico.js
+++ backend/src/usuario/usuario-servico.js
@@ -22,7 +22,7 @@ export const UserService = {
 
         try {
 
-            const data = await pool.query(`SELECT * FROM usuarios WHERE id = ${email}`)
+            const data = await pool.query(`SELECT * FROM usuarios WHERE email = ${email}`)
             return data.rows[0];
 
         } catch(e) {
