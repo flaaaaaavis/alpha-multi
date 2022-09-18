7c9acc8b6e09e19c62cc569b9c9dacb0b40bfa3d
Rodrigo <rodrigolfranca@gmail.com>
2022-09-18 17:17:26 -0300

    minor fix: strings

--- backend/src/usuario/usuario-servico.js
+++ backend/src/usuario/usuario-servico.js
@@ -8,7 +8,7 @@ export const UserService = {
 
         try {
 
-            const data = await pool.query(`SELECT * FROM usuarios WHERE id = ${id}`)
+            const data = await pool.query(`SELECT * FROM usuarios WHERE id = '${id}'`)
             return data.rows[0];
 
         } catch(e) {
@@ -22,7 +22,7 @@ export const UserService = {
 
         try {
 
-            const data = await pool.query(`SELECT * FROM usuarios WHERE email = ${email}`)
+            const data = await pool.query(`SELECT * FROM usuarios WHERE email = '${email}'`)
             return data.rows[0];
 
         } catch(e) {
@@ -71,7 +71,7 @@ export const UserService = {
 
         try {
 
-            const data = await pool.query(`DELETE FROM usuarios WHERE id = ${id}`)
+            const data = await pool.query(`DELETE FROM usuarios WHERE id = '${id}'`)
             return data.rows;
 
         } catch(e) {
