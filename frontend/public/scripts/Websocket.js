95cd2fd73fba2bd8adf397997b2d621771c887b3
plinio figueiredo dos santos <plinio.figueiredo.s@gmail.com>
2022-09-27 09:52:49 -0300

    feat: atualização do banco

--- frontend/public/scripts/Websocket.js
+++ frontend/public/scripts/Websocket.js
@@ -1,4 +1,5 @@
-export const ws = new WebSocket("ws://207.246.126.10:8000");
+//export const ws = new WebSocket("ws://207.246.126.10:8000");
+export const ws = new WebSocket("ws://localhost:8000");
 export let sala = localStorage.getItem("@dm-kanban:id");
 
 export function udpateSala() {
